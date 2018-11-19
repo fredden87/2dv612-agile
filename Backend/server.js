011fd6e01bd9ae6858d693140b8ba58505c54725
Fredrik Norrman <fredden87@gmail.com>
2018-11-19 07:56:43 +0100

    Coded the base for the rest api

---
+++ Backend/server.js
@@ -0,0 +1,7 @@
+const http = require('http')
+const app = require('./app')
+
+const port = process.env.PORT || 3000
+const server = http.createServer(app)
+console.log('server running on port: ' + port)
+server.listen(port)
