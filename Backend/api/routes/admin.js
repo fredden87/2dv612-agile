2d7b33977f1637934a72b385e421298297b7ca15
Kirill Bacha <kb222rh@student.lnu.se>
2018-11-19 17:50:11 +0100

    remove console logging and deprecation fix

--- Backend/api/routes/admin.js
+++ Backend/api/routes/admin.js
@@ -1,31 +1,40 @@
 const express = require('express')
 const router = express.Router()
 const typemodel = require('../db_resources/typemodel.js')
+// uncomment relevant lines to switch into post mode when frontend is ready
+
 router.get('/', (req, res, next) => {
-  // Temp test
-  if (true) {
- // if (req.body.name === 'admin' && req.body.password === 'secret' && req.body.type) {
-    res.status(200).json({
-      message: 'pass'
-    })
+// router.post('/', (req, res, next) => {  
+
+  if (true) { 
+  // if(req.body.name === 'admin' && req.body.password === 'secret' && req.body.type) { // TODO DO NOT STORE PASSWORD AND USERNAME HERE
     const mongoose = require('mongoose')
     //you will need to have a PASS=mongoDBpassword environment variable to connect!
-    mongoose.connect('mongodb+srv://team3:'+process.env.PASS+'@cluster0-xwlga.mongodb.net/team3', function(error){
+    mongoose.connect('mongodb+srv://team3:'+process.env.PASS+'@cluster0-xwlga.mongodb.net/team3', { useNewUrlParser: true }, function(error){
     if(error){ 
-        console.log(error)
+        res.status(500).json({
+          message: 'Unable to establish database connection'
+        })
       }
-      typemodel.create({type:'test'},function(err){
+      let input="ok"
+      //let input=req.body.type
+
+      //TODO  check if already present
+      typemodel.create({type:input},function(err){
         if(err){
-          console.log("failed writing to db")
+          res.status(500).json({
+            message: 'unable to create database object'
+          })
         }
-        console.log("connection successful, type saved")
+        res.status(200).json({
+          message: 'successfuly created type in database'
+        })
       })
     })
   } else {
     res.status(401).json({
       message: 'fail'
     })
-    console.log('fail')
   }
 })
 
