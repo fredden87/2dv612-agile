be305f3ee5398de0894831ab01cede8a57e39c53
Fredrik Norrman <fredden87@gmail.com>
2018-11-19 10:57:07 +0100

    added login route and error handling

---
+++ Backend/api/routes/login.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+router.post('/', (req, res, next) => {
+  // temp test
+  if (req.body.name === 'admin' && req.body.password === 'secret') {
+    res.status(200).json({
+      message: 'pass'
+    })
+    console.log('pass')
+  } else {
+    res.status(401).json({
+      message: 'fail'
+    })
+    console.log('fail')
+  }
+})
+
+module.exports = router
--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-Parser')
 
+const loginRoutes = require('./api/routes/login')
+// app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// routes
+app.use('/login', loginRoutes)
+
+// error handling needs to be after all routes
 app.use((req, res, next) => {
-  res.status(200).json({
-    message: 'Test!'
+  const error = new Error('Not found!')
+  error.status = 404
+  next(error)
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  res.json({
+    error: {
+      message: error.message
+    }
   })
 })
 
