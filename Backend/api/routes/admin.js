bf6bbaa0b40e29ca19979fed9d1d774410b041d2
Kirill Bacha <kb222rh@student.lnu.se>
2018-11-19 17:27:08 +0100

    refactor and primitive write to db

--- Backend/api/routes/admin.js
+++ Backend/api/routes/admin.js
@@ -1,19 +1,26 @@
 const express = require('express')
 const router = express.Router()
-
+const typemodel = require('../db_resources/typemodel.js')
 router.get('/', (req, res, next) => {
   // Temp test
   if (true) {
+ // if (req.body.name === 'admin' && req.body.password === 'secret' && req.body.type) {
     res.status(200).json({
       message: 'pass'
     })
     const mongoose = require('mongoose')
     //you will need to have a PASS=mongoDBpassword environment variable to connect!
-    mongoose.connect('mongodb+srv://team3:'+process.env.PASS+'@cluster0-xwlga.mongodb.net/admin', function(error){
-      if(error){ console.log(error)}
-      console.log("connection successful")
+    mongoose.connect('mongodb+srv://team3:'+process.env.PASS+'@cluster0-xwlga.mongodb.net/team3', function(error){
+    if(error){ 
+        console.log(error)
+      }
+      typemodel.create({type:'test'},function(err){
+        if(err){
+          console.log("failed writing to db")
+        }
+        console.log("connection successful, type saved")
+      })
     })
-
   } else {
     res.status(401).json({
       message: 'fail'
