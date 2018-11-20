5f07a9d4ee2c1d63251661cd9346ee0c9d985507
Kirill Bacha <kb222rh@student.lnu.se>
2018-11-20 19:41:34 +0100

    cors

--- Frontend/src/views/Register.vue
+++ Frontend/src/views/Register.vue
@@ -54,7 +54,7 @@ export default {
       event.preventDefault()
       let request=new XMLHttpRequest()
       request.open('POST', 'http://127.0.0.1:3000/user/signup', false)
-      request.setRequestHeader('Content-Type', 'application/json')
+      request.setRequestHeader('Access-Control-Allow-Headers','Content-Type', 'application/json')
       let formData=new FormData()
       formData.append("password", document.getElementById("password1").value)
       formData.append("email", document.getElementById("email").value)
