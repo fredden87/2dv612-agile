1290531cb02bd47d39885e1157eae8b74b81cbe2
Kirill Bacha <kb222rh@student.lnu.se>
2018-11-20 19:40:01 +0100

    debugging formdata

--- Frontend/src/views/Register.vue
+++ Frontend/src/views/Register.vue
@@ -53,8 +53,8 @@ export default {
     registerUser: function (event) {
       event.preventDefault()
       let request=new XMLHttpRequest()
-      request.setRequestHeader('Content-Type', 'application/json')
       request.open('POST', 'http://127.0.0.1:3000/user/signup', false)
+      request.setRequestHeader('Content-Type', 'application/json')
       let formData=new FormData()
       formData.append("password", document.getElementById("password1").value)
       formData.append("email", document.getElementById("email").value)
