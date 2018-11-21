388b3533449ec677eefc0bfd27162f6c37f949e8
margvo <mg222ym@student.lnu.se>
2018-11-21 14:39:17 +0100

    Added login frontend

---
+++ Frontend/src/views/Login.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="login-wrapper">
+    <h4 class="text">Please login to proceed</h4>
+    <div class="section"></div>
+    <div>
+      <form class="col s8" method="post">
+        <div class="input-field col s6">
+          <input id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="password" type="password" class="validate">
+          <label for="password">Password</label>
+        </div>
+        <button
+          class="btn
+          waves-effect waves-light"
+          type="submit"
+          name="btn_login"
+          v-on:click="login">
+          Login
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    components: {
+
+    },
+    methods: {
+      login: function (event) {
+        event.preventDefault()
+        const request=require('request')
+        request.post('http://127.0.0.1:3000/user/login', {
+          form:{password:document.getElementById("password").value,
+            email: document.getElementById("email").value }
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+  .login-wrapper {
+    width: 50%;
+    margin: 0 auto;
+  }
+</style>
