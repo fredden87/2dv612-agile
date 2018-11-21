3e8334b88140e39762cb843b7ea3a799544f76e0
margvo <mg222ym@student.lnu.se>
2018-11-21 21:17:37 +0100

    Added email validation

--- Backend/api/db_resources/usermodel.js
+++ Backend/api/db_resources/usermodel.js
@@ -23,7 +23,13 @@ const userSchema = new Schema({
         enum: ['Admin', 'Employee', 'Park owner', 'Car owner'],
         required: true
     },
-    email: { type: String, required: true, unique: true },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/},
     password: { type: String, required: true },
     confirmPassword: { type: String, required: true }
 })
