cd7915728e0cf2aa07ac1b16a063c8b025d860f5
Fredrik Norrman <fredden87@gmail.com>
2018-11-21 10:19:48 +0100

    format to follow standard.js rules and some refactoring

--- Backend/api/db_resources/usermodel.js
+++ Backend/api/db_resources/usermodel.js
@@ -1,14 +1,14 @@
 'use strict'
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    firstname: { type: String },
-    lastname: { type: String },
-    role: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+  _id: mongoose.Schema.Types.ObjectId,
+  firstname: { type: String },
+  lastname: { type: String },
+  role: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 })
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema)
--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,14 @@
-require('dotenv').config();
+require('dotenv').config()
 
 const http = require('http')
 const app = require('./app')
-let mailModel = require('./mail');
-
+const mailModel = require('./mail')
 
 const port = process.env.PORT || 3000
-const server = http.createServer(app).listen(port)
+http.createServer(app).listen(port)
 console.log('server running on port: ' + port)
 
 // sends the mail
 // if this lines are uncommented, the welcome email will send everytime you start server
-  // mailModel.sendMail(() => {
-  // })
+// mailModel.sendMail(() => {
+// })
