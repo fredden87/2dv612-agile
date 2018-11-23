ac2ca721b1c9eda906461dffd8690a45f18fa2ed
margvo <mg222ym@student.lnu.se>
2018-11-23 12:25:24 +0000

    Fixed usermodel

--- Backend/api/db_resources/usermodel.js
+++ Backend/api/db_resources/usermodel.js
@@ -30,7 +30,7 @@ const userSchema = new Schema({
         required: true,
         unique: true,
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/},
-    password: { type: String, required: true },
+        password: { type: String, required: true }
 })
 
 module.exports = mongoose.model('User', userSchema);
