b5fbf14a696647acd0c28c615a0ae905bc1ff84a
Fredrik Norrman <fredden87@gmail.com>
2018-11-28 15:50:45 +0100

    /vehicle route added

--- Backend/api/routes/vehicle.js
+++ Backend/api/routes/vehicle.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
 const User = require('../db_resources/usermodel.js')
 const MONGODB_URL = 'mongodb+srv://team3:' + process.env.PASS + '@cluster0-xwlga.mongodb.net/team3'
 const router = express.Router()
-const jwt = require('jsonwebtoken')
 
-router.post('/', (req, res, next) => {
+router.patch('/', (req, res, next) => {
   connectDB(res)
+  User.findOne({ email: req.body.email })
+    .exec()
+    .then(user => {
+      user.vehicle = req.body.car
+      user.save()
+      return res.status(200).json({
+        message: 'New vehicle added'
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        error: err
+      })
+    })
 })
 
 function connectDB (res) {
@@ -21,3 +34,5 @@ function connectDB (res) {
     })
   })
 }
+
+module.exports = router
