585cc709dfb8d4ee2b4a152dd343b8a31c67a56d
margvo <mg222ym@student.lnu.se>
2018-11-28 16:37:10 +0000

    Added saving session

--- Backend/api/routes/login.js
+++ Backend/api/routes/login.js
@@ -26,9 +26,11 @@ router.post('/', (req, res, next) => {
         if (result) {
          // if (!req.session.email && !req.session.password) {
             console.log(req.session)
-            req.session.cookie.email = req.body.email
+            req.session.isLoggedIn = true;
+            //req.session.cookie.email = req.body.email
             req.session.email = req.body.email;
             req.session.password = req.body.password;
+            req.session.save()
          // }
           let token = jwt.sign({ id: user.id }, process.env.PASS, { expiresIn: 86400 })
           res.status(200).json({ message: 'Welcome: '+user[0].firstname+' '+user[0].lastname, auth: true, token: token, user: user[0] })
