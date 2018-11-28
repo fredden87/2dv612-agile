71f2e22ea6f1abbe2a03741c9dfaea2be1a1e296
Popniten <pe222gq@student.lnu.se>
2018-11-28 15:59:13 +0100

    Fetching data for car registration

--- Frontend/src/router.js
+++ Frontend/src/router.js
@@ -5,6 +5,7 @@ import Register from './views/Register.vue'
 import Login from './views/Login.vue'
 import Welcome from './views/Welcome.vue'
 import Admin from './views/Admin.vue'
+import UserSettings from './views/UserSettings.vue'
 
 Vue.use(Router)
 
@@ -32,6 +33,11 @@ let router = new Router({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/usersettings',
+      name: 'usersettings',
+      component: UserSettings
     },
     {
       path: '/welcome',
--- Frontend/src/views/UserSettings.vue
+++ Frontend/src/views/UserSettings.vue
@@ -1,12 +1,38 @@
 <template>
   <div>
-    User settings here...
+    <div class="row">
+      <form class="col s12">
+        <div class="row">
+          <div class="input-field col s6">
+            <input id="car_reg" type="text" class="validate">
+            <label for="car_reg">Vehicle registration number</label>
+          </div>
+        </div>
+        <button
+          class="btn
+          waves-effect waves-light"
+          type="submit"
+          name="action"
+          v-on:click="addVehicle">
+          Register
+        </button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "UserSettings"
+  name: "UserSettings",
+  methods: {
+    addVehicle: function(event) {
+      event.preventDefault()
+      const carRegistration = document.getElementById("car_reg").value
+      const userEmail = JSON.parse(localStorage.getItem('user')).email
+      console.log(carRegistration)
+      console.log(userEmail)
+    }
+  }
 }
 </script>
 
