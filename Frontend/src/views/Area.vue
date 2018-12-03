a2c383b78d8097ae3ca70fc30080e95823d8e0ba
Kirill Bacha <kb222rh@student.lnu.se>
2018-12-03 20:56:13 +0100

    table for parking area

--- Frontend/src/views/Area.vue
+++ Frontend/src/views/Area.vue
@@ -135,17 +135,19 @@ selectorData()
     viewArea: function(event){
       let instance = document.getElementById('areaOpt')
       let selected=instance.options[instance.selectedIndex]
-      let renderView= document.getElementById('parkview')
+      let renderView= document.createElement('table')
+      renderView.class="minimalistBlack"
+      document.getElementById('parkview').appendChild(renderView)
       for (let i=0; i < selected.lat; i++){
-        let newRow= document.createAttribute('div')
+        let newRow= document.createElement('tr')
         newRow.class="row"
+        renderView.appendChild(newRow)
         for (let j=0; j < selected.long; j++){
-          let newCol= document.createAttribute('div')
-          newCol.class="input-field col s"+(j+1)
+          let newCol= document.createElement('td')
+          newCol.class="col"
           newCol.textContent=(j+1)
           newRow.appendChild(newCol)
         }
-        renderView.appendChild(newRow)
       }
       window.M.toast({
       html: selected.value ,
@@ -162,4 +164,39 @@ selectorData()
     width: 50%;
     margin: 0 auto;
   }
+table.minimalistBlack {
+  border: 3px solid #000000;
+  width: 100%;
+  text-align: left;
+  border-collapse: collapse;
+}
+table.minimalistBlack td, table.minimalistBlack th {
+  border: 1px solid #000000;
+  padding: 5px 4px;
+}
+table.minimalistBlack tbody td {
+  font-size: 13px;
+}
+table.minimalistBlack thead {
+  background: #CFCFCF;
+  background: -moz-linear-gradient(top, #dbdbdb 0%, #d3d3d3 66%, #CFCFCF 100%);
+  background: -webkit-linear-gradient(top, #dbdbdb 0%, #d3d3d3 66%, #CFCFCF 100%);
+  background: linear-gradient(to bottom, #dbdbdb 0%, #d3d3d3 66%, #CFCFCF 100%);
+  border-bottom: 3px solid #000000;
+}
+table.minimalistBlack thead th {
+  font-size: 15px;
+  font-weight: bold;
+  color: #000000;
+  text-align: left;
+}
+table.minimalistBlack tfoot {
+  font-size: 14px;
+  font-weight: bold;
+  color: #000000;
+  border-top: 3px solid #000000;
+}
+table.minimalistBlack tfoot td {
+  font-size: 14px;
+}
 </style>
