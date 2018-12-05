3280e9578f7ed7423bdcbf2aadbe4bec280526ba
Kirill Bacha <kb222rh@student.lnu.se>
2018-12-05 15:53:19 +0100

    stringify?

--- Frontend/src/views/Area.vue
+++ Frontend/src/views/Area.vue
@@ -59,7 +59,7 @@ if (process.env.VUE_APP_ENVIRONMENT==="production"){
 
   let selectorData= function(){
      const user = JSON.parse(localStorage.getItem('user'))
-      request.post({uri: 'http://'+backendUrl+'/area', form: {sessionToken: user.sessionToken, email: JSON.parse(sessionStorage.getItem('email'))}}, function(err,response,body){
+      request.post({uri: 'http://'+backendUrl+'/area', form: {sessionToken: JSON.stringify(user.sessionToken), email: JSON.parse(sessionStorage.getItem('email'))}}, function(err,response,body){
   let data=JSON.parse(body)
   let area=document.getElementById('areaOpt')
   while (area.childNodes.length>1){
