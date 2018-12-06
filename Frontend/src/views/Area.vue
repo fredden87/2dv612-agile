2fb6ea0485d975855ce1104465f04700ac765450
Kirill Bacha <kb222rh@student.lnu.se>
2018-12-06 11:55:21 +0100

    empty registry fix

--- Frontend/src/views/Area.vue
+++ Frontend/src/views/Area.vue
@@ -75,6 +75,7 @@ if (process.env.VUE_APP_ENVIRONMENT==="production"){
   while (area.childNodes.length>1){
 area.removeChild(area.lastChild)
   }
+  if (data){
   data.forEach(function(item){
    let opt = document.createElement('option')
    opt.value=item.area.name
@@ -84,6 +85,7 @@ area.removeChild(area.lastChild)
    area.appendChild(opt)
   })
   M.FormSelect.init(document.getElementById('areaOpt'))
+}
   })
   }
 export default {
