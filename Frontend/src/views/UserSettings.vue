1cf958e88facf36eb249fe06bdf06be07b96374e
margvo <mg222ym@student.lnu.se>
2018-12-07 03:17:23 +0100

    Modified vehicle route

--- Frontend/src/views/UserSettings.vue
+++ Frontend/src/views/UserSettings.vue
@@ -47,6 +47,7 @@
 </template>
 
 <script>
+  import router from '../router'
   const request = require('request')
   let backendUrl = '127.0.0.1:3000'
   if (process.env.VUE_APP_ENVIRONMENT==="production"){
@@ -63,7 +64,7 @@
       data.forEach(function(item){
         let opt = document.createElement('option')
         //opt.value = document.getElementById("car_reg").value
-        opt.value=item.user.vehicle.type
+        opt.value=item.vehicle
         opt.textContent=opt.value
         vehicle.appendChild(opt)
       })
@@ -71,138 +72,128 @@
       M.FormSelect.init(document.getElementById('vehicleOpt'))
     })
   }
-export default {
-  name: "UserSettings",
-  mounted() {
-    // <li class="collection-item">Alvin</li>
-    const user = JSON.parse(localStorage.getItem('user'))
-    if (user !== null) {
-      // Logged in
-      const listElement = document.getElementById('vehicleList')
+  export default {
+    name: "UserSettings",
+      mounted() {
+        // <li class="collection-item">Alvin</li>
+        const user = JSON.parse(localStorage.getItem('user'))
+        if (user !== null) {
+          // Logged in
+          const listElement = document.getElementById('vehicleList')
 
-      let html = ''
-      user.vehicle.forEach(element => {
-        html += '<li class="collection-item">' + element + "</li>"
-      });
+          let html = ''
+          user.vehicle.forEach(element => {
+            html += '<li class="collection-item">' + element + "</li>"
+          });
 
-      listElement.innerHTML = html
+          listElement.innerHTML = html
 
-    } else {
-      // Not logged in
-    }
-  },
-  mounted(){
-    selectorData()
-  },
-  methods: {
-    removeVehicle: function(event){
-      event.preventDefault()
-      let backendUrl = "127.0.0.1:3000";
-      if (process.env.VUE_APP_ENVIRONMENT === "production") {
-        backendUrl = "194.47.206.226:3000";
-      }
-
-      let instance = document.getElementById('vehicleOpt')
-      let selected=instance.options[instance.selectedIndex]
-      const vehicleNum = selected.value
-      const user = JSON.parse(localStorage.getItem('user'))
-      const data = {email: user.email, vehicle: vehicleNum}
-      console.log(data)
-      fetch("http://" + backendUrl + "/vehicle/remove", {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)})
-        .then((response) => {
-          if (response.status === 200) {
-            // Display success message
-            //selectorData()
-            window.M.toast({
-              html: "Vehicle was removed",
-              classes: 'green darken-1'
-            })
-          } else {
-            // Display error message
-            window.M.toast({
-              html: "Vehicle could not be removed",
-              classes: 'deep-orange accent-4 black-text',
-              displayLength: 6000
-            })
-          }
-        })
+        } else {
+          // Not logged in
+        }
+      },
+    mounted(){
+      selectorData()
     },
-    addVehicle: function(event) {
-      event.preventDefault()
-      const carRegistration = document.getElementById("car_reg").value
-      const user = JSON.parse(localStorage.getItem('user'))
-
-      if (user !== null) {
-        const userEmail = user.email
-        const data = {email: userEmail, car: carRegistration}
-
+    methods: {
+      removeVehicle: function(event){
+        event.preventDefault()
         let backendUrl = "127.0.0.1:3000";
         if (process.env.VUE_APP_ENVIRONMENT === "production") {
           backendUrl = "194.47.206.226:3000";
         }
 
-        fetch("http://" + backendUrl + "/vehicle", {
-          method: 'PATCH',
+        let instance = document.getElementById('vehicleOpt')
+        let selected=instance.options[instance.selectedIndex]
+        const vehicleNum = selected.value
+        const user = JSON.parse(localStorage.getItem('user'))
+        const data = {email: user.email, vehicle: vehicleNum}
+
+        fetch("http://" + backendUrl + "/vehicle/remove", {
+          method: 'POST',
           headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
           },
           body: JSON.stringify(data)})
-        .then((response) => {
-          if (response.status === 200) {
-            // Display success message
-            window.M.toast({
-              html: "Vehicle was added",
-              classes: 'green darken-1'
-            })
+          .then((response) => {
+            if (response.status === 200) {
+              // Display success message
+              selectorData()
+              window.M.toast({
+                html: "Vehicle was removed",
+                classes: 'green darken-1'
+              })
+            } else {
+              // Display error message
+              window.M.toast({
+                html: "Vehicle could not be removed",
+                classes: 'deep-orange accent-4 black-text',
+                displayLength: 6000
+              })
+            }
+          })
+      },
+      addVehicle: function(event) {
+        event.preventDefault()
+        const carRegistration = document.getElementById("car_reg").value
+        const user = JSON.parse(localStorage.getItem('user'))
 
-            const listElement = document.getElementById('vehicleList')
+        if (user !== null) {
+          const userEmail = user.email
+          const data = {email: userEmail, vehicle: carRegistration}
 
-            let html = '<li class="collection-item">' + carRegistration.toUpperCase() +'</li>'
-
-            listElement.innerHTML += html
-
-            let vehicleSelect=document.getElementById('vehicleOpt')
-
-
-            //data.forEach(function(item){
-              let opt = document.createElement('option')
-              opt.value=carRegistration.toUpperCase()
-              opt.textContent=opt.value
-              vehicleSelect.appendChild(opt)
-            //})
-
-            M.FormSelect.init(document.getElementById('vehicleOpt'))
-          } else {
-            // Display error message
-            window.M.toast({
-              html: "Vehicle could not be added",
-              classes: 'deep-orange accent-4 black-text',
-              displayLength: 6000
-            })
+          let backendUrl = "127.0.0.1:3000";
+          if (process.env.VUE_APP_ENVIRONMENT === "production") {
+            backendUrl = "194.47.206.226:3000";
           }
-        })
-      } else {
-        window.M.toast({
-          html: "Vehicle could not be added, are you logged in?",
-          classes: 'deep-orange accent-4 black-text',
-          displayLength: 6000
-        })
+
+          fetch("http://" + backendUrl + "/vehicle", {
+            method: 'PATCH',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)})
+            .then((response) => {
+              if (response.status === 200) {
+                // Display success message
+                selectorData()
+                window.M.toast({
+                  html: "Vehicle was added",
+                  classes: 'green darken-1'
+                })
+
+                const listElement = document.getElementById('vehicleList')
+
+                let html = '<li class="collection-item">' + carRegistration.toUpperCase() +'</li>'
+
+                listElement.innerHTML += html
+
+              } else {
+                // Display error message
+                window.M.toast({
+                  html: "Vehicle could not be added",
+                  classes: 'deep-orange accent-4 black-text',
+                  displayLength: 6000
+                })
+              }
+            })
+        } else {
+          window.M.toast({
+            html: "Vehicle could not be added, are you logged in?",
+            classes: 'deep-orange accent-4 black-text',
+            displayLength: 6000
+          })
+        }
       }
     }
   }
-}
 </script>
 
 <style>
-.register-wrapper {
-  width: 60%;
-  margin: 0 auto;
-}
+  .register-wrapper {
+    width: 60%;
+    margin: 0 auto;
+  }
 </style>
