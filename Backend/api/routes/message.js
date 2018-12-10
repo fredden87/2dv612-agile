a0b42a0bdad90e18bbbd7a5db06ae0ba6570e87a
Popniten <pe222gq@student.lnu.se>
2018-12-10 18:45:05 +0100

    Connecting routes to express.

--- Backend/api/routes/message.js
+++ Backend/api/routes/message.js
@@ -5,12 +5,40 @@ const Messagemodel = require('../db_resources/messagemodel.js')
 /**
  * Get the admin message.
  */
-router.get('/', (req, res, next) => {})
+router.get('/', (req, res, next) => {
+  res.status(200).json({message: 'hej'})
+})
 
 /**
  * Set a new message. Empties `viewed_by` array.
  */
-router.post('/', (req, res, next) => {})
+router.post('/', (req, res, next) => {
+  const mongoose = require('mongoose')
+
+  mongoose.connect('mongodb+srv://team3:' + process.env.PASS + '@cluster0-xwlga.mongodb.net/team3', { useNewUrlParser: true }, function (error) {
+    if (error) {
+      res.status(500).json({
+        message: 'Unable to establish database connection'
+      })
+    }
+    // Updates the message in database, creates if not exists
+    let query = {stringId: "admin_message"}
+    let update = { messages: { message: req.body.message, viewed_by: [], stringId: 'admin_message' } }
+    let options = { upsert: true, new: true, setDefaultsOnInsert: true }
+
+    Messagemodel.update(query, {$push: update}, options, function (err, res) {
+      if (err) {
+        res.status(500).json({
+          message: 'Unable to update database.'
+        })
+      } else {
+        res.status(200).json({
+          message: 'Message updated.'
+        })
+      }
+    })
+  })
+})
 
 /**
  * Updates `viewed_by` array.
