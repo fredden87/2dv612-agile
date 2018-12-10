3f56a30c74b1d0438515cb3d75c6e3c1feb12edb
Popniten <pe222gq@student.lnu.se>
2018-12-10 20:25:39 +0100

    Implementing PATCH /message to update viewed list.

--- Backend/api/routes/message.js
+++ Backend/api/routes/message.js
@@ -33,6 +33,7 @@ router.get('/', (req, res, next) => {
       if (err) {
         console.log(err)
       } else {
+        mongoose.connection.close()
         res.status(200).json(result)
       }
     })
@@ -87,6 +88,53 @@ router.post('/', (req, res, next) => {
  * 500: DB Connection error.
  *
  */
-router.patch('/', (req, res, next) => {})
+router.patch('/', (req, res, next) => {
+  const mongoose = require('mongoose')
+
+  mongoose.connect('mongodb+srv://team3:' + process.env.PASS + '@cluster0-xwlga.mongodb.net/team3', { useNewUrlParser: true }, function (error) {
+    if (error) {
+      res.status(500).json({
+        message: 'Unable to establish database connection'
+      })
+    }
+
+    Messagemodel.findOne({stringId: 'admin_message'}, function (err, result) {
+      if (err) {
+        console.log(err)
+      } else {
+        //console.log(result)
+        //console.log(req.body.email)
+        //console.log(result)
+        result.save((err) => {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log('You saved!')
+          }
+        })
+        //let list = result.viewed_by
+        //list.push(req.body.email)
+        //mongoose.connection.close()
+        //res.status(200).json(result)
+      }
+    })
+    .then(data => {
+      data.viewed_by.push(req.body.email)
+      console.log(data)
+
+      Messagemodel.findByIdAndUpdate(
+        data._id,
+        data,
+        { new: true },
+        (err, message) => {
+          if (err) {
+            return res.status(500).send(err)
+          }
+          return res.status(200).json(message)
+        }
+      )
+    })
+  })
+})
 
 module.exports = router
