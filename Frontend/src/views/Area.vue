5f1f18c71f0f2049dbecfd26d1747a65181d2325
Kirill Bacha <kb222rh@student.lnu.se>
2018-12-10 20:20:40 +0100

    number as string?

--- Frontend/src/views/Area.vue
+++ Frontend/src/views/Area.vue
@@ -238,7 +238,7 @@ newCell.appendChild(edit)
         console.log('_____')
         let value = selected.timezones[timezone]
 switch(timezone){
-  case 0:
+  case '0':
   console.log('case 0 fired')
     document.getElementById('row4cell1').textContent='0'+hour+':00-0'+hour+':59'
     document.getElementById('row4edit1').value=value
