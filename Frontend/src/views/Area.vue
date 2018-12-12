9f785bd56172edcfc5dbdaafd2b85ee2f8bd30b3
Kirill Bacha <kb222rh@student.lnu.se>
2018-12-12 18:33:28 +0100

    remove placeholder

--- Frontend/src/views/Area.vue
+++ Frontend/src/views/Area.vue
@@ -6,17 +6,17 @@
         </div>
         <div class="row">
           <div class="input-field col s6">
-            <input placeholder=" " id="aname" type="text" class="validate">
+            <input id="aname" type="text" class="validate">
             <label for="aname">Area Name</label>
           </div>
         </div>
         <div class="row">
           <div class="input-field col s6">
-            <input placeholder=" " id="long" type="number" class="validate">
+            <input id="long" type="number" class="validate">
             <label for="long">Longitude</label>
           </div>
           <div class="input-field col s6">
-            <input placeholder=" " id="lat" type="number" class="validate">
+            <input id="lat" type="number" class="validate">
             <label for="lat">Latitude</label>
           </div>
         </div>
