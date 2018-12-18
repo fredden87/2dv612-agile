01947fb9bc617584c0a41e2caadc51015d094431
Kirill Bacha <kb222rh@student.lnu.se>
2018-12-18 16:15:34 +0100

    no idea

--- Frontend/vue.config.js
+++ Frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
  // will probably need to adapt this to production...
     devServer: {
       open: process.platform === 'darwin',
-     // host: '0.0.0.0',
+      host: '0.0.0.0',
       port: 8080,
       https: process.env.NODE_ENV === 'production'
       ? true
@@ -11,6 +11,6 @@ module.exports = {
     },
       baseUrl: process.env.NODE_ENV === 'production'
         ? 'https://194.47.206.229:8080/'
-        : 'http://127.0.0.1:8080/'
+        : '/'
     
   }
