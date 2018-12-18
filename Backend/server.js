c8edc161ebe810403c2929cd5bc3f5ef3138364c
Kirill Bacha <kb222rh@student.lnu.se>
2018-12-18 01:06:15 +0100

    have to go with https anyway

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,17 @@
 require('dotenv').config()
-// const fs = require('fs')
-// const https = require('https')
+const fs = require('fs')
+const https = require('https')
 const http = require('http')
 const app = require('./app')
 const mailModel = require('./mail')
 
 const port = process.env.PORT || 3000
-http.createServer(app).listen(port)
-console.log('server running on port: ' + port)
-// var privateKey = fs.readFileSync('key.pem')
-// var certificate = fs.readFileSync('cert.pem')
+// http.createServer(app).listen(port)
+// console.log('server running on port: ' + port)
+var privateKey = fs.readFileSync('key.pem')
+var certificate = fs.readFileSync('cert.pem')
 
-// https.createServer({
-//   key: privateKey,
-//   cert: certificate
-// }, app).listen(port)
+https.createServer({
+  key: privateKey,
+  cert: certificate
+}, app).listen(port)
