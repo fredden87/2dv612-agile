66646236b0e3bb68ba9a83680cae0ba81b243041
Kirill Bacha <kb222rh@student.lnu.se>
2018-12-18 02:00:03 +0100

    fullchain not ok in express

--- Backend/server.js
+++ Backend/server.js
@@ -8,9 +8,9 @@ const mailModel = require('./mail')
 const port = process.env.PORT || 3000
 // http.createServer(app).listen(port)
 // console.log('server running on port: ' + port)
-var privateKey = fs.readFileSync('/etc/letsencrypt/live/cscloud482.lnu.se/fullchain.pem', 'utf8')
-var certificate = fs.readFileSync('/etc/letsencrypt/live/cscloud482.lnu.se/privkey.pem', 'utf8')
-
+var privateKey = fs.readFileSync('/etc/letsencrypt/live/cscloud482.lnu.se/privkey.pem', 'utf8')
+var certificate = fs.readFileSync('/etc/letsencrypt/live/cscloud482.lnu.se/cert.pem', 'utf8')
+var chain = fs.readFileSync('/etc/letsencrypt/live/cscloud482.lnu.se/chain.pem', 'utf8')
 // - Congratulations! Your certificate and chain have been saved at:
 // /etc/letsencrypt/live/cscloud482.lnu.se/fullchain.pem
 // Your key file has been saved at:
