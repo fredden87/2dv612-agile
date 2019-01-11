f45d197630f4231376885358c6b2f9b060292f40
Kirill Bacha <kb222rh@student.lnu.se>
2019-01-11 13:16:50 +0100

    https on frontend?

--- Frontend/vue.config.js
+++ Frontend/vue.config.js
@@ -1,16 +1,14 @@
 module.exports = {
- // will probably need to adapt this to production...
-    devServer: {
-      open: process.platform === 'darwin',
-      host: '0.0.0.0',
-      port: 8080,
-      https: process.env.NODE_ENV === 'production'
-      ? true
-      : false,
-      hotOnly: false,
-    },
-      baseUrl: process.env.NODE_ENV === 'production'
-        ? 'https://194.47.206.229:8080/'
-        : '/'
-    
-  }
+  // will probably need to adapt this to production...
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 8080,
+    https: process.env.VUE_APP_ENVIRONMENT === 'production',
+    hotOnly: false
+  },
+  baseUrl: process.env.VUE_APP_ENVIRONMENT === 'production'
+    ? 'https://194.47.206.229:8080/'
+    : '/'
+
+}
