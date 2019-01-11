dd0a03d1e8e58e3930f644db765f54e2d3681aa7
Kirill Bacha <kb222rh@student.lnu.se>
2019-01-11 13:09:01 +0100

    module.exports on backend

--- Backend/api/backendURL.js
+++ Backend/api/backendURL.js
@@ -2,4 +2,4 @@
    const backendUrl= process.env.VUE_APP_ENVIRONMENT === 'production'
     ? 'https://cscloud482.lnu.se'
     : 'http://127.0.0.1:3000'
-export {backendUrl}
+module.exports=backendUrl
--- Backend/api/frontendURL.js
+++ Backend/api/frontendURL.js
@@ -2,4 +2,4 @@
    const frontendUrl= process.env.VUE_APP_ENVIRONMENT === 'production'
     ? 'https://194.47.206.229:8080'
     : 'http://127.0.0.1:8080'
-export {frontendUrl}
+module.exports=frontendUrl
